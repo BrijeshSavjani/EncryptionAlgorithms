34672ae56d0b40063ff0e2c2a585893baea986ff
BrijeshSavjani <21848@mynsb.co.uk>
2021-02-06 10:21:57 +0000

    Add Block based encryption
    Now same amount of chars can be returned.

--- Polynomial Ceaser Cipher.py
+++ Polynomial Ceaser Cipher.py
@@ -1,5 +1,6 @@
+import math
 numerical_values = []
-encrypted_text = []
+encrypted_text = ""
 text = input("Please enter text>")
 unecncrypted = list(text)
 invalid = True
@@ -13,6 +14,22 @@ def getIncrease(n):
         a+= (int(coefficient)) * (int(n)**(int(x)-int(i)))
         i+=1
     return a
+def SplitIntoChunks(master_text,chunk_size):
+    Chunks =[]
+    Chunk = ""
+    i = 0
+    amount_of_chunks = math.ceil(len(master_text)/chunk_size)
+    while (len(Chunks)<= amount_of_chunks):
+        while i < chunk_size:
+            try:
+                Chunk += master_text[i]
+            except:
+                break
+            i +=1
+        chunk_size += chunk_size
+        Chunks.append(Chunk)
+        Chunk = ""
+    return Chunks
 while(invalid):
     invalid = False
     password_input= input("Pincode (atleast 4 digits)>")
@@ -22,13 +39,21 @@ while(invalid):
     if len(password) != int(password[0]) + 2:
         print("Pincode must be two digits longer then the first number")
         invalid = True
-
+    try:
+        block_legnth = int(input("Block legnth"))
+        if block_legnth > len(text):
+            invalid = True
+    except: invalid = True
 #First letter = order,2nd letter = b,3rd letter = c and so on.. where f(n) = an^x + bn^(x-1) + c^(x-2) .... [letter]^(x-x)
 for number in password[1:]:
     coefficients.append(number)
-x = password[0]
-n = 0
-for char in unecncrypted:
-        n += 1
-        encrypted_text.append(ord(char) + getIncrease(n))
+chunks = SplitIntoChunks(text,block_legnth)
+chunks.remove("")
+for chunk in chunks:
+    n = 0
+    for char in chunk:
+            n += 1
+            encrypted_text += chr(ord(char) + int(getIncrease(n)))
+       
+    
 print(encrypted_text)
