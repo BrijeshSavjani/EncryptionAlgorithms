ee8bd7dcde4ec0a1f31306aeaca487bb5703178d
BrijeshSavjani <21848@mynsb.co.uk>
2021-02-10 02:01:00 +0000

    Polynomial ceaser cupher can now decrypt code in blocks
    Polynomial ceaser cupher can now decrypt code in blocks

--- Polynomial Ceaser Cipher Decryptor.py
+++ Polynomial Ceaser Cipher Decryptor.py
@@ -1,17 +1,36 @@
+import math
+
 numerical_values = []
 decrypted_text = ""
 text = input("Please enter text>")
 encrypted = text.split(',')
 invalid = True
-x = 0
 coefficients = []
+
 def getIncrease(n):
     a = 0
     i = 0
     for coefficient in coefficients:
-        a+= (int(coefficient)) * (int(n)**(int(x)-int(i)))
+        a+= (int(coefficient)) * (int(n)**(int(password[0])-int(i)))
         i+=1
     return a
+
+def SplitIntoChunks(master_text,chunk_size):
+    Chunks =[]
+    Chunk = ""
+    i = 0
+    amount_of_chunks = math.ceil(len(master_text)/chunk_size)
+    while (len(Chunks)<= amount_of_chunks):
+        while i < chunk_size:
+            try:
+                Chunk += master_text[i]
+            except:
+                break
+            i +=1
+        chunk_size += chunk_size
+        Chunks.append(Chunk)
+        Chunk = ""
+    return Chunks
 
 while(invalid):
     invalid = False
@@ -22,13 +41,20 @@ while(invalid):
     if len(password) != int(password[0]) + 2:
         print("Pincode must be one digit longer then the first number")
         invalid = True
+    try:
+        block_legnth = int(input("Block legnth"))
+        if block_legnth > len(text):
+            invalid = True
+    except: invalid = True
 for number in password[1:]:
     coefficients.append(number)
-x = password[0]
-n = 0
-for char in encrypted:
-        n += 1
-        decrypted_text += chr(int(char) - getIncrease(n))
+chunks = SplitIntoChunks(text,block_legnth)
+chunks.remove("")
+for chunk in chunks:
+    n = 0
+    for char in chunk:
+            n += 1
+            decrypted_text += chr(ord(char) - getIncrease(n))
 print(decrypted_text)
 
 
