95dddea985152ce65266cf0b486e887af23e681b
BrijeshSavjani <21848@mynsb.co.uk>
2021-02-12 03:29:44 +0000

    Now loops in overflow!

--- Polynomial Ceaser Cipher Decryptor.py
+++ Polynomial Ceaser Cipher Decryptor.py
@@ -9,16 +9,16 @@ coefficients = []
 
 def getIncrease(n):
     a = 0
-    i = 0
+    i = int(password[0])
     for coefficient in coefficients:
-        a+= (int(coefficient)) * (int(n)**(int(password[0])-int(i)))
-        i+=1
+        a+= (float(coefficient)) * (int(n)**(int(password[0])-int(i)))
+        i-=1
     return a
-
 def SplitIntoChunks(master_text,chunk_size):
     Chunks =[]
     Chunk = ""
     i = 0
+    orignal_chunk_size = chunk_size
     amount_of_chunks = math.ceil(len(master_text)/chunk_size)
     while (len(Chunks)<= amount_of_chunks):
         while i < chunk_size:
@@ -27,10 +27,11 @@ def SplitIntoChunks(master_text,chunk_size):
             except:
                 break
             i +=1
-        chunk_size += chunk_size
+        chunk_size = chunk_size + orignal_chunk_size
         Chunks.append(Chunk)
         Chunk = ""
     return Chunks
+
 
 while(invalid):
     invalid = False
@@ -54,7 +55,7 @@ for chunk in chunks:
     n = 0
     for char in chunk:
             n += 1
-            decrypted_text += chr(ord(char) - getIncrease(n))
+            decrypted_text += chr(ord(char) -  int(getIncrease(n)))
 print(decrypted_text)
 
 
--- Polynomial Ceaser Cipher.py
+++ Polynomial Ceaser Cipher.py
@@ -8,15 +8,16 @@ coefficients = []
 
 def getIncrease(n):
     a = 0
-    i = 0
+    i = int(password[0])
     for coefficient in coefficients:
         a+= (float(coefficient)) * (int(n)**(int(password[0])-int(i)))
-        i+=1
+        i-=1
     return a
 def SplitIntoChunks(master_text,chunk_size):
     Chunks =[]
     Chunk = ""
     i = 0
+    orignal_chunk_size = chunk_size
     amount_of_chunks = math.ceil(len(master_text)/chunk_size)
     while (len(Chunks)<= amount_of_chunks):
         while i < chunk_size:
@@ -25,10 +26,14 @@ def SplitIntoChunks(master_text,chunk_size):
             except:
                 break
             i +=1
-        chunk_size += chunk_size
+        chunk_size = chunk_size + orignal_chunk_size
         Chunks.append(Chunk)
         Chunk = ""
     return Chunks
+def returnIncrease(a,max_x):
+    while a > max_x:
+        a = a - max_x
+    return a
 while(invalid):
     invalid = False
     password_input= input("Pincode (atleast 4 digits)>")
@@ -47,12 +52,13 @@ while(invalid):
 for number in password[1:]:
     coefficients.append(number)
 chunks = SplitIntoChunks(text,block_legnth)
-chunks.remove("")
+try: chunks.remove("")
+except: pass
 for chunk in chunks:
     n = 0
     for char in chunk:
             n += 1
-            encrypted_text += chr(ord(char) + int(getIncrease(n)))
+            encrypted_text += chr(ord(char) + int(returnIncrease(getIncrease(n),(1114111 - ord(char)))))
        
     
 print(encrypted_text)
